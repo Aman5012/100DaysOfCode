06e99566670f65a489e9fd7e9f9cbf1915de86fd
Aman Rao <124618381+Aman5012@users.noreply.github.com>
2025-07-13 22:26:36 +0530

    Create  Maximum Matching of Players With Trainers.cpp

---
+++ Day43/Maximum Matching of Players With Trainers.cpp
@@ -0,0 +1,38 @@
+2410. Maximum Matching of Players With Trainers
+//QUESTION LNK: https://leetcode.com/problems/maximum-matching-of-players-with-trainers/?envType=daily-question&envId=2025-07-13
+
+TOPIC: array, two Pointer, greedy, sorting
+
+
+CODE:
+
+// Time Complexity: O(n lon n + m log m)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        int m = players.size();
+        int n = trainers.size();
+
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        int ans=0;
+        int p=0;
+        int t=0;
+        while(p<m && t<n){
+            if(players[p]<= trainers[t]){
+                ans++;
+                p++;
+                t++;
+            }
+            else if(players[p]> trainers[t]) {
+                t++;
+            }
+            else{
+                p++;
+            }
+        }
+        return ans;
+    }
+};
