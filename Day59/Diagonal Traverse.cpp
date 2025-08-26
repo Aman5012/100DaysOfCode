5fa22a7ba56bfe7e730b1726c45e52e7822bb0c9
Aman Rao <124618381+Aman5012@users.noreply.github.com>
2025-08-26 18:11:02 +0530

    Refine topic and complexity comments in Diagonal Traverse
    Updated topic classification and commented time/space complexity.

---
+++ Day59/Diagonal Traverse.cpp
@@ -0,0 +1,41 @@
+498. Diagonal Traverse
+//QUESTION LINK: https://leetcode.com/problems/diagonal-traverse/?envType=daily-question&envId=2025-08-25
+
+TOPIC: Array
+Matrix
+Simulation
+
+
+CODE:
+// Time Complexity = O(m·n)
+// Space Complexity = O(m·n)
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        unordered_map<int, vector<int>> ans;
+        for(int r=0; r<m; r++){
+            for(int c=0; c<n; c++){
+                int x= r+c;
+                ans[x].push_back(mat[r][c]);
+            }
+        }
+        vector<int> res;
+        for(auto& it: ans){
+            if(it.first%2==0){
+                reverse(it.second.begin(), it.second.end());
+            }
+        }
+        for(int key=0; key<=(m-1+n-1); key++){
+            auto vec= ans[key];
+
+            for(auto it: vec ){
+                res.push_back(it);
+            }
+        }
+    
+        return res;
+    }
+};
