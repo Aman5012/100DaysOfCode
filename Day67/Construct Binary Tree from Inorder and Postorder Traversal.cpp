3e6ee3464822d9bb4c7fe8be390ba13b4c738157
Aman Rao <124618381+Aman5012@users.noreply.github.com>
2025-10-06 22:18:38 +0530

    Fix comment syntax for complexity in C++ file
    Updated comments to use C++ style for time and space complexity.

---
+++ Day67/Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -0,0 +1,54 @@
+106. Construct Binary Tree from Inorder and Postorder Traversal
+//QUESTION LINK: https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+TOPIC: Array
+Hash Table
+Divide and Conquer
+Tree
+Binary Tree
+
+
+CODE:
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> mpp;
+
+        for(int i=0; i<inorder.size(); i++){
+            mpp[inorder[i]]=i;
+        }
+
+        int preIndex = 0;
+        return build(preorder, inorder, mpp, preIndex, 0, preorder.size()-1);
+    }
+
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, unordered_map<int, int>& mpp, int& preIndex, int s, int e){
+        if(s>e) return nullptr;
+            // pick the root value from preorder
+        int rootVal = preorder[preIndex++];
+            // creating new node
+        TreeNode* root = new TreeNode(rootVal);
+        
+
+        int index = mpp[rootVal];
+
+        root->left = build(preorder, inorder, mpp, preIndex, s, index-1);
+        root->right = build(preorder, inorder, mpp, preIndex, index+1, e);
+        return root;
+    }
+};
