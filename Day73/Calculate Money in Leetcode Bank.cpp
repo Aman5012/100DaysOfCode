c58c6fd5b8b665fcf45ab367f50fea20536ad231
Aman Rao <124618381+Aman5012@users.noreply.github.com>
2025-10-30 18:12:56 +0530

    Add solution for Calculate Money in Leetcode Bank
    Implement two approaches to calculate total money in Leetcode Bank based on given days.

---
+++ Day73/Calculate Money in Leetcode Bank.cpp
@@ -0,0 +1,45 @@
+1716. Calculate Money in Leetcode Bank
+//QUESTION LINK: https://leetcode.com/problems/calculate-money-in-leetcode-bank/description/?envType=daily-question&envId=2025-10-25
+
+TOPIC: Math
+
+
+CODE:
+///////////////////  appraoch 1 ////////////////////
+class Solution {
+public:
+    int totalMoney(int n) {
+        int terms = n/7; // total compelte week
+        int first =28;  // in one week
+        int last  = 28 + (terms-1)*7; 
+        int res = terms*(first + last)/2;
+        
+        int remain = n%7;
+        int start = terms + 1;
+        for(int d=1; d<= remain; d++){
+            res += start;
+            start++;
+        }
+        
+        return res;;
+    }
+};
+
+
+///////////////////  appraoch 2 ////////////////////
+class Solution {
+public:
+    int totalMoney(int n) {
+        int totalAmount=0;
+        int Monday=1;
+        
+        while(n>0){
+            for(int day=0; day< min(n, 7); day++){
+                totalAmount += Monday + day;
+            }
+            n -=7;
+            Monday++;
+        }
+        return totalAmount;
+    }
+};
